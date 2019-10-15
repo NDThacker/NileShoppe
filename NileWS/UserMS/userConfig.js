f132df2a930fd53de943b6f14b18816f753c27e8
Naman Thacker <namanthacker1965@gmail.com>
2019-10-15 12:23:31 +0530

    Adding files

---
+++ NileWS/UserMS/userConfig.js
@@ -0,0 +1,12 @@
+{
+    "hydra": {
+        "serviceName": "users",
+        "serviceIP": "",
+        "servicePort": 1000,
+        "serviceType": "",
+        "serviceDescription": "This is the service of user",
+        "redis": {
+            "url": "redis://127.0.0.1:6379/15"
+        }
+    }
+}
