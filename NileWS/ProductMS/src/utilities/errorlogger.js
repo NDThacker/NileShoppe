bfae5f0c832ac361f44c82001e7ba315257b61b7
Naman Thacker <namanthacker1965@gmail.com>
2019-10-15 12:19:30 +0530

    Adding files

---
+++ NileWS/ProductMS/src/utilities/errorlogger.js
@@ -0,0 +1,18 @@
+const fs = require( 'fs' );
+
+let errorLogger = ( err, req, res, next ) => {
+
+	fs.appendFile( 'ErrorLogger.txt', err.stack + " " + new Date(), ( error ) => {
+		if( error )
+			{ console.log(error); }
+	} )
+	if( err.status )
+		res.status( err.status );
+	else{
+		res.status( 500 );
+
+	}
+	res.json( { "message": err.message } )
+	next();
+}
+module.exports = errorLogger;
